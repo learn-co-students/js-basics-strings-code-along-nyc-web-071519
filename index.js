1ccdc8eb691b2c4b50f486f40ff9f39790dd630d
Luis Ascencio <luismiguelascencio@gmail.com>
2019-08-23 10:08:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+const currentUser = "Luis";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.substr(0, 1)}!`;
